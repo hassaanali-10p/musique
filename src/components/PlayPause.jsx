3fc4c1071af470b7538c55991287cc68c3e89cc0
Hassaan Ali <hassaan.ali@10pearls.com>
2023-01-31 14:23:53 +0500

    playpause implementation

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,9 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle className="text-gray-300" size={35} onClick={handlePause} />
+  ) : (
+    <FaPlayCircle className="text-gray-300" size={35} onClick={handlePlay} />
+  );
 export default PlayPause;
