85c0bfa96a4dded8e52f0410d1f61e6aba39df77
Hassaan Ali <hassaan.ali@10pearls.com>
2023-02-01 21:24:42 +0500

    add top artists section

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,20 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import { useNavigate } from "react-router-dom";
+
+const ArtistCard = ({ song }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="flex flex-col w-[250px] p-4 animate-slideup cursor-pointer items-center"
+      onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}>
+      <img
+        src={song?.images?.background}
+        alt="name"
+        className="rounded-full w-full object-cover"></img>
+      <p className="mt-4 font-semibold text-lg text-white truncate">
+        {song?.subtitle}
+      </p>
+    </div>
+  );
 };
 
 export default ArtistCard;
--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,28 @@
-const TopArtists = () => <div>TopArtists</div>;
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { ArtistCard, Error, Loader, SongCard } from "../components";
 
-export default TopArtists;
+const TopCharts = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading songs around you..." />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Top Artists
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <ArtistCard key={song?.key} song={song} i={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TopCharts;
